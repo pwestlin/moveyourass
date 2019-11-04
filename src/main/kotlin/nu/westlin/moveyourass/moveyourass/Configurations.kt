93a32b0db6e905040255fe36967947f3a33fdf86
Peter Vestlin <peter.vestlin@lm.se>
2019-11-04 15:55:13 +0100

    Reading from application.yml

--- src/main/kotlin/nu/westlin/moveyourass/moveyourass/Configurations.kt
+++ src/main/kotlin/nu/westlin/moveyourass/moveyourass/Configurations.kt
@@ -11,33 +11,12 @@ import org.springframework.fu.kofu.configuration
 import org.springframework.transaction.ReactiveTransactionManager
 import org.springframework.transaction.reactive.TransactionalOperator
 
-private fun transactionalOperator(txMgr: ReactiveTransactionManager): TransactionalOperator {
-    return TransactionalOperator.create(txMgr)
-}
-
-private fun r2dbcTransactionManager(connectionFactory: ConnectionFactory): R2dbcTransactionManager {
-    return R2dbcTransactionManager(connectionFactory)
-}
-
-private fun h2ConnectionConfiguration(): H2ConnectionConfiguration {
-    // TODO petves: Read url/user/password from properties file
-    return H2ConnectionConfiguration.builder().url("mem:test;DB_CLOSE_DELAY=-1").build()
-}
-
-private fun h2ConnectionFactory(h2ConnectionConfiguration: H2ConnectionConfiguration): H2ConnectionFactory {
-    return H2ConnectionFactory(h2ConnectionConfiguration)
-}
-
-private fun databaseClient(h2ConnectionFactory: H2ConnectionFactory): DatabaseClient {
-    return DatabaseClient.builder().connectionFactory(h2ConnectionFactory).build()
-}
-
-
 val dataConfig = configuration {
     beans {
         // I can't use the below DSL because it has no transaction configuration and I want that
         //r2dbcH2()
-        bean { h2ConnectionConfiguration() }
+        configurationProperties<H2R2dbcProperties>(prefix = "r2dbc.h2")
+        bean(::h2ConnectionConfiguration)
         bean(::h2ConnectionFactory)
         bean(::r2dbcTransactionManager)
         bean(::transactionalOperator)
@@ -56,7 +35,8 @@ val dataConfig = configuration {
 val initDatabaseWithDataConfig = configuration {
     listener<ApplicationReadyEvent> {
         runBlocking {
-            ref<UserRepository>().setupTestData()
+            val a =
+                ref<UserRepository>().setupTestData()
         }
     }
 }
@@ -64,3 +44,24 @@ val initDatabaseWithDataConfig = configuration {
 val webConfig = configuration {
     // TODO petves: Impl
 }
+
+private fun transactionalOperator(txMgr: ReactiveTransactionManager): TransactionalOperator {
+    return TransactionalOperator.create(txMgr)
+}
+
+private fun r2dbcTransactionManager(connectionFactory: ConnectionFactory): R2dbcTransactionManager {
+    return R2dbcTransactionManager(connectionFactory)
+}
+
+private fun h2ConnectionConfiguration(configurationProperties: H2R2dbcProperties): H2ConnectionConfiguration {
+    return H2ConnectionConfiguration.builder().url(configurationProperties.url).build()
+}
+
+private fun h2ConnectionFactory(h2ConnectionConfiguration: H2ConnectionConfiguration): H2ConnectionFactory {
+    return H2ConnectionFactory(h2ConnectionConfiguration)
+}
+
+private fun databaseClient(h2ConnectionFactory: H2ConnectionFactory): DatabaseClient {
+    return DatabaseClient.builder().connectionFactory(h2ConnectionFactory).build()
+}
+
---
+++ src/main/kotlin/nu/westlin/moveyourass/moveyourass/Properties.kt
@@ -0,0 +1,3 @@
+package nu.westlin.moveyourass.moveyourass
+
+data class H2R2dbcProperties(val url: String, val user: String? = null, val password: String? = null)
