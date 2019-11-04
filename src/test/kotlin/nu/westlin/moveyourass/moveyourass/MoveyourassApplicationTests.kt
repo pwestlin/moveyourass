051624057a0e138d9c2d2c4606fb6859feae4744
Peter Vestlin <peter.vestlin@lm.se>
2019-11-04 13:44:09 +0100

    first commit

---
+++ src/test/kotlin/nu/westlin/moveyourass/moveyourass/MoveyourassApplicationTests.kt
@@ -0,0 +1,13 @@
+package nu.westlin.moveyourass.moveyourass
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoveyourassApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
